fa7fc06aa005bbcfabb3f5eb7cf9f7361385c96d
Jonathan Lu <jonlu@ucdavis.edu>
2017-07-14 12:52:39 -0700

    Reword app landing page

--- packages/react-scripts/template/src/scenes/App/App.js
+++ packages/react-scripts/template/src/scenes/App/App.js
@@ -13,18 +13,15 @@ class App extends Component {
           <li>Redux dev and prod suite</li>
           <li>React Router v4</li>
           <li>True JS Hot Module Replacement</li>
-          <li>Absolute Path for imports</li>
-          <li>Less and Sass for webpack</li>
+          <li>Absolute path for imports</li>
+          <li>Less and Sass webpack support</li>
           <li>StandardJS Linting</li>
           <li>Lodash, moment, classnames</li>
           <li>And much more!</li>
         </ul>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <Link className="linkText" to='/Example'>Redux Example</Link>
         </p>
-        <p className="App-intro"></p>
-        <Link className="linkText" to='/Example'>Redux Example</Link>
-
       </div>
     )
   }
